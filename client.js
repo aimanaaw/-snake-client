9a238fc4db8fcfea4ca2619d7a4db823e28aaa8c
Aiman Azmie <aimanaaw@gmail.com>
2019-09-25 19:33:47 +0000

    Snake Game

--- client.js
+++ client.js
@@ -5,15 +5,35 @@ const net = require('net');
  */
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '172.46.2.204',
+    host: 'localhost',
     port: 50541
   });
+  if (conn) {
+    console.log("Successfully connected to the game server\n")
+  };
+  conn.on('connect', () => {
+    console.log("hello")
+      setTimeout(() => conn.write("Move: left"), 100)
+      setTimeout(() => conn.write("Move: down"), 200)
+      setTimeout(() => conn.write("Move: left"), 300)
+      setTimeout(() => conn.write("Move: up"), 400)
+      setTimeout(() => conn.write("Move: right"), 500)
+  });
+  
+  // Direction commands
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
+
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
-  
+  const myFunction = (data) => {
+    console.log(data);
+ }
   conn.on('data', (data) => {
-     console.log(data);
-  });
+    console.log(data);
+ });
 
 }
 
